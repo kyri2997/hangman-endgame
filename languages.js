b919fb13702c5d42b091373b0a03f3a07cc04eea
kyri2997 <rousoukyri@gmail.com>
2025-06-04 16:49:12 +0100

    Upload

---
+++ languages.js
@@ -0,0 +1,47 @@
+export const languages = [
+    {
+        name: "Iron Man",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Captain America",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Black Panther",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Hulk",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Spider-Man",
+        backgroundColor: "#298EC6",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Hulk",
+        backgroundColor: "#599137",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Doctor Strange",
+        backgroundColor: "#FFD742",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Ant Man",
+        backgroundColor: "#D02B2B",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Thanos Wins",
+        backgroundColor: "#2D519F",
+        color: "#F9F4DA",
+    },
+]
